44dba4ec164af2bd5fa0adf79f1a242b0defa241
alpheratz0 <alpheratz99@protonmail.com>
2022-09-03 19:50:36 -0300

    add man pages

--- install.sh
+++ install.sh
@@ -4,12 +4,17 @@ if test -z "${PREFIX}"; then
 	PREFIX=/usr/local
 fi
 
+MANPREFIX="${PREFIX}/share/man"
+
 cd src
 
 mkdir -p "${DESTDIR}${PREFIX}/bin"
+mkdir -p "${DESTDIR}${MANPREFIX}/man1"
 
 for f in *; do
 	echo "installing ${f}..."
 	cp "${f}" "${DESTDIR}${PREFIX}/bin"
 	chmod 755 "${DESTDIR}${PREFIX}/bin/${f}"
+	echo "installing man page for ${f}..."
+	cp "../man/${f}.1" "${DESTDIR}${MANPREFIX}/man1"
 done
